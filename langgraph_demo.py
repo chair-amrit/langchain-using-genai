a421cd98c847f954e589033c5f2b36045ff8535e
chair-amrit <ishereamrit@gmail.com>
2026-06-21 12:09:44 +0530

    added multiple values in state and used .stream to check working of nodes in each step

--- langgraph_demo.py
+++ langgraph_demo.py
@@ -2,39 +2,52 @@ from typing import TypedDict
 from langgraph.graph import StateGraph, START, END
 
 #LangGraph needs to know what data exists in the state.
-class state(TypedDict):
-    text:str
+class State(TypedDict):
+    question:str
+    answer:str
+    steps_taken:list
 
-#It does nothing just to see how data flow.
-def node1(state):
-    print("node 1:",state)
-    return state
 
-#This modifies the state.
-def node2(state):
-    print("node 2 :",state)
+#Add question
+def node1(State):
     return {
-        "text":state["text"].upper()
+        "steps_taken":State["steps_taken"]+["question_received"]
+    }
+
+#add answer update steps taken
+def node2(State):
+    return {
+        "answer":f"Answer for {State['question']}",
+        "steps_taken":State["steps_taken"]+["answer_generated"]
+    }
+
+#complete update steps taken
+def node3(State):
+    return {
+        "steps_taken" : State["steps_taken"]+["completed"]
     }
 
 #create graqh
-graph = StateGraph(state)
+graph = StateGraph(State)
 
 #addnodes
 graph.add_node("node1",node1)
 graph.add_node("node2",node2)
+graph.add_node("node3",node3)
 
 #connect nodes
 graph.add_edge(START,"node1")
 graph.add_edge("node1","node2")
-graph.add_edge("node2",END)
+graph.add_edge("node2","node3")
+graph.add_edge("node3",END)
 
 #Convert flowchart into executable program
 app=graph.compile()
 
-#run
-result=app.invoke({
-    "text":"hello world"
-})
-
-print(result)
+#use .stream
+for event in app.stream({
+    "question":"What is LoRA?",
+    "answer":"",
+    "steps_taken":[]
+}):
+    print(event)
